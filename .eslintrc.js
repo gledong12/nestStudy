82feaf586010af3b628647aa787859728c69d4e2
이동근 <eagle5424228@gmail.com>
2023-12-02 22:43:42 +0900

    Feat: UsersController

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
+    'eslint-disable-next-line '
   ],
   root: true,
   env: {
@@ -21,5 +22,7 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": "error"
   },
 };
---
+++ src/users/users.controller.ts
@@ -0,0 +1,9 @@
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Post, Put, Query, Req, Res } from '@nestjs/common';
+import { Request, Response } from 'express';
+import { CreateCatDto, UpdateCatDto } from './dto';
+
+@Controller('users')
+export class UsersController {
+  
+}
+
