9204a53ce0c5f9145df1ea0089c3947a0378fdd4
이동근 <eagle5424228@gmail.com>
2023-12-04 22:23:55 +0900

    Feat: 12/2/4 module, env

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,22 @@
+import { ConfigService } from '@nestjs/config';
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly configService:ConfigService
+    ) {}
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    // return this.appService.getHello();
+    return process.env.DATABASE_HOST;
+  }
+
+  @Get('/db-host-from-config')
+  getDatabaseHostFromConfigService(): string {
+    return this.configService.get('DATABASE_HOST')
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,23 @@
+import { validationSchema } from './config/validationSchema';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersController } from './users/users.controller';
-import { UsersService } from './users/users.service';
-import { EmailService } from './email/email.service';
+import { UsersModule } from './users/users.module';
+import { EmailModule } from './email/email.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import emailConfig from './config/emailConfig';
 
 @Module({
-  imports: [],
-  controllers: [AppController, UsersController],
-  providers: [AppService, UsersService, EmailService],
+  imports: [
+    UsersModule, 
+    EmailModule, 
+    ConfigModule.forRoot({
+    envFilePath: [`${__dirname}/config/env/.${process.env.NODE_ENV}.env`],
+    load: [emailConfig],
+    isGlobal: true,
+    validationSchema,
+  })],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
